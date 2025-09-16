35d93cb646ecc0cf2cd285b8a2d6233525a82831
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-09-15 21:58:55 -0500

    Enhance hormone and unit data structures by introducing NonBinary gender, refining Unit interface, and adding UnitValue and Conversion types for improved clarity and type safety.

--- src/lib/logic/units.ts
+++ src/lib/logic/units.ts
@@ -6,14 +6,29 @@ export enum HormoneName {
 }
 
 export enum Gender {
+  // both cis and trans
   Female = "female",
   Male = "male",
-  Undefined = "undefined",
+
+  NonBinary = "non-binary",
 }
 
-interface Unit {
-  name: string;
+export enum UnitName {
+  nmol_L = "nmol/L",
+  ng_dL = "ng/dL",
+  ng_mL = "ng/mL",
+  pg_mL = "pg/mL",
+  pmol_L = "pmol/L",
+}
+
+export interface Unit {
+  name: UnitName;
   symbol?: string;
+}
+
+export interface UnitValue {
+  value: number;
+  unit: Unit;
 }
 
 interface Range {
@@ -22,10 +37,18 @@ interface Range {
   unit: Unit;
 }
 
-interface Hormone {
-  id: HormoneName;
-  name: string;
-  level: number;
-  unit: Unit;
+export interface Hormone {
+  id: HormoneName; // hormone name
+  value: UnitValue;
   referenceRange?: Range; // this changes on multiple factors
 }
+
+export type Conversion = {
+  name: ConvertionName;
+  from: UnitName;
+  to: UnitName;
+  factor: number;
+};
+
+// A preset represents a hormonal target for an specific group, we plan to save them
+export interface PresetName {}
